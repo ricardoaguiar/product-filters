46de08f47aab1b1fa7fb64578a5b4e58032a8500
Bence A. Tóth <tothab@gmail.com>
2021-11-15 19:29:19 +0100

    Selected filters component

--- src/App.js
+++ src/App.js
@@ -26,10 +26,47 @@ const FilterDropdown = ({
   </>
 );
 
+const SelectedFilters = ({ selectedItems, setSelectedItems }) => (
+  <>
+    {selectedItems.map((selectedItem) => (
+      <div key={selectedItem}>
+        {selectedItem}
+        <button
+          onClick={() => {
+            setSelectedItems(
+              selectedItems.filter((item) => item !== selectedItem)
+            );
+          }}
+        >
+          X
+        </button>
+      </div>
+    ))}
+  </>
+);
+
 const App = () => {
   const [selectedColors, setSelectedColors] = useState([]);
   const [selectedSizes, setSelectedSizes] = useState([]);
   const [selectedAvailabilities, setSelectedAvailabilities] = useState([]);
+
+  const filteredProducts = products.filter((product) => {
+    const matchesSelectedColors =
+      selectedColors.some((color) => product.colors.includes(color)) ||
+      selectedColors.length === 0;
+    const matchesSelectedSizes =
+      selectedSizes.some((size) => product.sizes.includes(size)) ||
+      selectedSizes.length === 0;
+    const matchesSelectedAvailabilities =
+      selectedAvailabilities.some((availability) =>
+        product.availability.includes(availability)
+      ) || selectedAvailabilities.length === 0;
+    return (
+      matchesSelectedColors &&
+      matchesSelectedSizes &&
+      matchesSelectedAvailabilities
+    );
+  });
 
   return (
     <div className="App">
@@ -51,13 +88,26 @@ const App = () => {
         selectedItems={selectedAvailabilities}
         setSelectedItems={setSelectedAvailabilities}
       />
-      {selectedColors.join("")}
-      {selectedSizes.join("")}
-      {selectedAvailabilities.join("")}
+      <SelectedFilters
+        selectedItems={selectedColors}
+        setSelectedItems={setSelectedColors}
+      />
+      <SelectedFilters
+        selectedItems={selectedSizes}
+        setSelectedItems={setSelectedSizes}
+      />
+      <SelectedFilters
+        selectedItems={selectedAvailabilities}
+        setSelectedItems={setSelectedAvailabilities}
+      />
       <main>
-        {products.map((product) => (
+        <div>{filteredProducts.length} items</div>
+        {filteredProducts.map((product) => (
           <div key={product.id}>
             <h2>{product.name}</h2>
+            <p>Colors: {product.colors.join(", ")}</p>
+            <p>Sizes: {product.sizes.join(", ")}</p>
+            <p>Stores: {product.availability.join(", ")}</p>
           </div>
         ))}
       </main>
