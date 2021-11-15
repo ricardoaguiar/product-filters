b55d68206d89e813d54d34bdf332502207969c9f
Bence A. Tóth <tothab@gmail.com>
2021-11-15 19:58:50 +0100

    Did a bit of refactoring

--- src/App.js
+++ src/App.js
@@ -9,24 +9,24 @@ const FilterDropdown = ({
   filteredProducts,
   propertyName,
 }) => {
-  let itemsInDropdown;
-  // If there is at least one selected color,
-  if (selectedItems.length > 0) {
-    // we want to have all colors in the dropdown,
-    // except for the ones which are selected
-    itemsInDropdown = allItems.filter((item) => !selectedItems.includes(item));
-  }
-  // If there is no color selected
-  else {
-    // we want to have only the colors in the dropdown,
-    // where at least one of the filtered products
-    // is available in one of the selected colors
-    itemsInDropdown = allItems.filter((item) => {
+  const itemsInDropdown = allItems.filter((item) => {
+    // E.g. if propertyName is "colors":
+    // If there is at least one selected color,
+    if (selectedItems.length > 0) {
+      // we want to have all colors in the dropdown,
+      // except for the ones which are selected
+      return !selectedItems.includes(item);
+    }
+    // If there is no color selected
+    else {
+      // we want to have only the colors in the dropdown,
+      // where at least one of the filtered products
+      // is available in one of the selected colors
       return filteredProducts.some((product) =>
         product[propertyName].some((property) => item === property)
       );
-    });
-  }
+    }
+  });
 
   return (
     <span className="filterDropdown">
